d659c216a1cf5a5e11c42766c93a7fd17c765092
ZhangJiaLei <zhang.jialei@outlook.com>
2020-12-11 18:29:41 +0800

    添加文件自动扫描脚本

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,33 +1,63 @@
-// 侧边菜单设置 
-module.exports = [
-    {
-        title: '入门',   // 必要的
-        path: '/Iot-doc/01 基础/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        collapsable: false, // 折叠,可选的, 默认值是 true, 
-        children: [
-          '/Iot-doc/01 基础/02 更新记录', 
-          '/Iot-doc/01 基础/03 常见名词'
-        ]
-      },
-      {
-        title: '安装部署',   // 必要的
-        path: '/Iot-doc/02 安装部署/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在 
-        sidebarDepth: 1,    // 可选的, 默认值是 1
-        children: [ 
-          '/Iot-doc/02 安装部署/01 源码部署', 
-          '/Iot-doc/02 安装部署/02 docker部署',
-          '/Iot-doc/02 安装部署/03 服务器nginx+docker部署'
-         ]
-      } ,
-      {
-        title: '操作手册',   // 必要的
-        path: '/Iot-doc/04 操作手册/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在 
-        sidebarDepth: 1,    // 可选的, 默认值是 1
-        children: [ 
-           "/Iot-doc/04 操作手册/01 菜单管理",
-           "/Iot-doc/04 操作手册/1 租户管理",
-           "/Iot-doc/04 操作手册/02 常场地管理"
-         ]
-      } ,
-]
- 
+module.exports = [ {title:'基础',
+path:'/Iot-doc/01 基础',
+collapsable:true,
+children: [
+'/Iot-doc/01 基础/02 更新记录.md', 
+'/Iot-doc/01 基础/03 常见名词.md', 
+],
+}
+,{title:'安装部署',
+path:'/Iot-doc/02 安装部署',
+collapsable:true,
+children: [
+'/Iot-doc/02 安装部署/01 源码部署.md', 
+'/Iot-doc/02 安装部署/02 docker部署.md', 
+'/Iot-doc/02 安装部署/03 服务器nginx+docker部署.md', 
+],
+}
+,{title:'进阶',
+collapsable:true,
+children: [
+'/Iot-doc/03 进阶/01 物模型说明.md', 
+'/Iot-doc/03 进阶/02 设备接入说明.md', 
+],
+}
+,{title:'操作手册',
+path:'/Iot-doc/04 操作手册',
+collapsable:true,
+children: [
+'/Iot-doc/04 操作手册/01 菜单管理.md', 
+'/Iot-doc/04 操作手册/02 场地中心.md', 
+'/Iot-doc/04 操作手册/03 规则管理.md', 
+'/Iot-doc/04 操作手册/04 监控大屏.md', 
+'/Iot-doc/04 操作手册/05 监控管理.md', 
+'/Iot-doc/04 操作手册/06 控件管理.md', 
+'/Iot-doc/04 操作手册/07 库存管理.md', 
+'/Iot-doc/04 操作手册/08 设备中心.md', 
+'/Iot-doc/04 操作手册/09 数据中心.md', 
+'/Iot-doc/04 操作手册/1 租户管理.md', 
+'/Iot-doc/04 操作手册/10 员工管理.md', 
+'/Iot-doc/04 操作手册/99 系统配置.md', 
+],
+}
+,{title:'开发手册',
+collapsable:true,
+children: [
+'/Iot-doc/05 开发手册/01 前端开发手册.md', 
+'/Iot-doc/05 开发手册/02 后端开发手册.md', 
+'/Iot-doc/05 开发手册/03 数据平台开发手册.md', 
+'/Iot-doc/05 开发手册/04 数据平台开发手册2.md', 
+'/Iot-doc/05 开发手册/05 协议开发说明.md', 
+'/Iot-doc/05 开发手册/06 设备注册.md', 
+'/Iot-doc/05 开发手册/07 设备管理.md', 
+'/Iot-doc/05 开发手册/08 规则引擎.md', 
+'/Iot-doc/05 开发手册/09 设备接入.md', 
+],
+}
+,{title:'常见问题',
+collapsable:true,
+children: [
+'/Iot-doc/06 常见问题/QA.md', 
+],
+}
+, ]
